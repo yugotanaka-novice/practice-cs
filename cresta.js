ef44310e4f58f628e3ae3ed4648baa853037bb51
yugotanaka-novice <121290640+yugotanaka-novice@users.noreply.github.com>
2022-12-23 19:56:21 +0900

    Update cresta.js

--- cresta.js
+++ cresta.js
@@ -1,1 +1,18 @@
 
+$(function(){
+  $('header,.aboutus, .topic-bottom,.latest,.contact,.vision,.company-list,.title').hide();
+  $('header, .topic-bottom,.latest,.contact,.vision,.company-list,.title').fadeIn(1500);
+  
+  $('.top-text').hide();
+  $('.top-text').fadeIn(3000);
+
+
+  
+  $(window).on("scroll",function(){
+    $('.aboutus,.ceo').slideDown(1000);
+    
+  });
+
+ 
+
+});
